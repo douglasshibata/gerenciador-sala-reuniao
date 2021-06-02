068a656bf46cc6b918bc678828fcbbdc141f71ca
Douglas Shibata <douglas.shibata11@gmail.com>
2021-06-02 19:22:13 -0300

    add crud of rooms

---
+++ client-room/src/app/pages/room/details-room/details-room.component.ts
@@ -0,0 +1,34 @@
+import { Room } from './../../../shared/model/room';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { RoomService } from 'src/app/shared/services/room.service';
+
+@Component({
+  selector: 'app-details-room',
+  templateUrl: './details-room.component.html',
+  styleUrls: ['./details-room.component.css']
+})
+export class DetailsRoomComponent implements OnInit {
+
+  id: number;
+  room: Room;
+
+  constructor(private route: ActivatedRoute,private router: Router,
+    private roomService: RoomService) { }
+
+  ngOnInit() {
+    this.room = new Room();
+
+    this.id = this.route.snapshot.params['id'];
+
+    this.roomService.getRoom(this.id)
+      .subscribe(data => {
+        console.log(data)
+        this.room = data;
+      }, error => console.log(error));
+  }
+
+  list(){
+    this.router.navigate(['rooms']);
+  }
+}
---
+++ client-room/src/app/pages/room/list-room/list-room.component.ts
@@ -0,0 +1,44 @@
+import { Router } from '@angular/router';
+import { RoomService } from './../../../shared/services/room.service';
+import { Room } from './../../../shared/model/room';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-list-room',
+  templateUrl: './list-room.component.html',
+  styleUrls: ['./list-room.component.css']
+})
+export class ListRoomComponent implements OnInit {
+
+  rooms: Observable<Room[]>;
+
+  constructor(private roomService: RoomService,
+    private router: Router) { }
+
+  ngOnInit() {
+    this.reloadData();
+  }
+
+  reloadData() {
+    this.rooms = this.roomService.getRoomsList();
+  }
+
+  deleteRoom(id: number) {
+    this.roomService.deleteRoom(id)
+      .subscribe(
+        data => {
+          console.log(data);
+          this.reloadData();
+        },
+        error => console.log(error));
+  }
+
+  roomDetails(id: number) {
+    this.router.navigate(['details', id]);
+  }
+
+  updateRoom(id: number) {
+    this.router.navigate(['update', id]);
+  }
+}
