18808e2125184c40d8624e9980bd33c5a5559ce5
Douglas Shibata <douglas.shibata11@gmail.com>
2021-06-02 14:58:53 -0300

    Add room model

---
+++ saladereuniao/src/main/java/com/digital/crud/saladereuniao/model/Room.java
@@ -0,0 +1,113 @@
+package com.digital.crud.saladereuniao.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "meetingroom")
+public class Room implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+
+	@Column(nullable = false)
+	private String name;
+
+	@Column(nullable = false)
+	private String date;
+
+	@Column(nullable = false)
+	private String startHour;
+
+	@Column(nullable = false)
+	private String endHour;
+
+	public Room() {
+	}
+
+	public Room(long id, String name, String date, String startHour, String endHour) {
+		this.id = id;
+		this.name = name;
+		this.date = date;
+		this.startHour = startHour;
+		this.endHour = endHour;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDate() {
+		return date;
+	}
+
+	public void setDate(String date) {
+		this.date = date;
+	}
+
+	public String getStartHour() {
+		return startHour;
+	}
+
+	public void setStartHour(String startHour) {
+		this.startHour = startHour;
+	}
+
+	public String getEndHour() {
+		return endHour;
+	}
+
+	public void setEndHour(String endHour) {
+		this.endHour = endHour;
+	}
+
+	@Override
+	public String toString() {
+		return "Room [id=" + id + ", name=" + name + ", date=" + date + ", startHour=" + startHour + ", endHour="
+				+ endHour + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (int) (id ^ (id >>> 32));
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Room other = (Room) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+
+}
