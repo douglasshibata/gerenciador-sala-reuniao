5a52c38eb776b3e68d2758bb1fd19ec916ceafa4
Douglas Shibata <douglas.shibata11@gmail.com>
2021-06-02 19:25:15 -0300

    add imports and first page

--- client-room/src/app/app.module.ts
+++ client-room/src/app/app.module.ts
@@ -1,14 +1,28 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { CreateRoomComponent } from './pages/room/create-room/create-room.component';
+import { DetailsRoomComponent } from './pages/room/details-room/details-room.component';
+import { ListRoomComponent } from './pages/room/list-room/list-room.component';
+import { UpdateRoomComponent } from './pages/room/update-room/update-room.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateRoomComponent,
+    DetailsRoomComponent,
+    ListRoomComponent,
+    UpdateRoomComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
